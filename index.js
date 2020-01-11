3603fd817f94fafbe22feb68f68103dafeb96640
Theodor Lindekaer <theodor.lindekaer@gmail.com>
2020-01-11 16:39:38 +0100

    Refactored dependency checks

--- index.js
+++ index.js
@@ -2,35 +2,37 @@ const path = require('path')
 
 require('dotenv').config({ path: path.join(__dirname, '.env') })
 
-const fs = require('fs')
 const execSync = require('child_process').execSync
 const _flow = require('lodash/fp/flow')
 const _mapValues = require('lodash/fp/mapValues')
 const github = require('octonode')
 const fileExists = require('file-exists')
 
-const TASK_FILE = path.join(__dirname, 'tasks.json')
+const TASK_FILE_PATH = path.join(__dirname, 'tasks.json')
 
-if (process.env.GITHUB_ACCESS_TOKEN == null || process.env.GITHUB_GIST_ID == null) {
-  console.error('Environment variables not provided')
-  process.exit()
-}
+verify([
+  {
+    failure: process.env.GITHUB_ACCESS_TOKEN == null,
+    message: 'Please provide a Github access token as env variable'
+  },
+  {
+    failure: process.env.GITHUB_GIST_ID == null,
+    message: 'Please provide a Github gist ID as env variable'
+  },
+  {
+    failure: !fileExists.sync(TASK_FILE_PATH),
+    message: 'No tasks.json found - please create it.'
+  }
+])
 
-if (!fileExists.sync(TASK_FILE)) {
-  console.error('Missing tasks.json')
-  process.exit()
-}
-
-const client = github.client(process.env.GITHUB_ACCESS_TOKEN).gist()
-const tasks = JSON.parse(fs.readFileSync(TASK_FILE))
-
-const runUpdate = async tasks => {
+const runTasksAndUpdateGists = async tasks => {
   // prettier-ignore
   const files = _flow(
     _mapValues(getCommandOutput),
     _mapValues(content => ({ content }))
   )(tasks)
 
+  const client = github.client(process.env.GITHUB_ACCESS_TOKEN).gist()
   await client.editAsync(process.env.GITHUB_GIST_ID, {
     description: `Backup from: ${new Date()}`,
     public: false,
@@ -45,6 +47,16 @@ const getCommandOutput = shellCmd =>
     .toString()
     .trim()
 
+function verify(requirements) {
+  requirements.forEach(req => {
+    if (req.failure === true) {
+      console.error(req.message)
+      process.exit()
+    }
+  })
+}
+
 // Run program
 // ==========================
-runUpdate(tasks)
+const tasks = require(TASK_FILE_PATH)
+runTasksAndUpdateGists(tasks)
